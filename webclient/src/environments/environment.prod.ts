b56866fb7d2876b1766ab25d09b521824c5e4732
Christopher Thonfeld-Guckes <ctg@triarc-labs.com>
2024-01-26 11:12:50 +0100

    fix: tenant-id and client-id swapped

--- webclient/src/environments/environment.prod.ts
+++ webclient/src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   production: true,
   title: window['env']['title'] || 'Metechno IO Database <i>UNCONFIGURED INSTANCE</i>',
   appRootUrl: location.origin,
-  azureClientId: '#AZURE_TENANT_ID#',
-  azureTenantId: '#AZURE_CLIENT_ID#'
+  azureClientId: '#AZURE_CLIENT_ID#',
+  azureTenantId: '#AZURE_TENANT_ID#'
 };
